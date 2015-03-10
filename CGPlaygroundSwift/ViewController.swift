2d782d9e7be73981d50649a084dbc1b45bdcee47
ccouturesitespect <ccouture@sitespect.com>
2015-03-10 07:15:25 -0400

    Initial Commit

---
+++ CGPlaygroundSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CGPlaygroundSwift
+//
+//  Created by Curtis Couture on 3/10/15.
+//  Copyright (c) 2015 BadBeanie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
